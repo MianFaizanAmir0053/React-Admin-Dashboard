db865de41cf2dfd8e68a0b6bcaa61d17162607e6
MianFaizanAmir0053 <97526215+MianFaizanAmir0053@users.noreply.github.com>
2023-08-01 15:38:56 +0500

    new

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,9 +10,32 @@ import PsychologyIcon from "@mui/icons-material/Psychology";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { Link, NavLink } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../Firebase/config";
+import { useDispatch, useSelector } from "react-redux";
+import { setError, setLoading, setUser } from "../redux/authSlice";
 
 function Sidebar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        localStorage.removeItem("user");
+        dispatch(setUser(null));
+        dispatch(setLoading(false));
+        dispatch(setError(null));
+
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className=" min-h-[100vh] border-r">
       <div className="text-bold py-[1.48rem] text-violet-700 font-semibold  min-[768px]:py-[0.95rem] text-center border-b">
@@ -23,27 +46,36 @@ function Sidebar() {
           <ul>
             <div>
               <p className="text-slate-400 text-xs ">MAIN</p>
-              <li className="flex  px-4 py-2 text-center">
+              <Link
+                to={"/"}
+                className="flex cursor-pointer hover:bg-slate-200 px-4 py-2 text-center"
+              >
                 <DashboardIcon className="text-violet-700 mr-2 p-[0.15rem]" />
                 <div>Dashboard</div>
-              </li>
+              </Link>
             </div>
 
             <div>
               <p className="text-slate-400 text-xs ">LISTS</p>
-              <li className="flex  px-4 py-2 text-center">
+              <Link
+                to={"/users"}
+                className="cursor-pointer hover:bg-slate-200 flex  px-4 py-2 text-center"
+              >
                 <GroupIcon className="text-violet-700 mr-2 p-[0.15rem]" />
                 <a href={"/users"}>Customers</a>
-              </li>
-              <li className="flex  px-4 py-2 text-center">
+              </Link>
+              <Link
+                to={"/products"}
+                className="cursor-pointer hover:bg-slate-200 flex  px-4 py-2 text-center"
+              >
                 <InventoryIcon className="text-violet-700 mr-2 p-[0.15rem]" />
                 <div>Inventory</div>
-              </li>
-              <li className="flex  px-4 py-2 text-center">
+              </Link>
+              <li className="cursor-pointer flex  px-4 py-2 text-center">
                 <WalletIcon className="text-violet-700 mr-2 p-[0.15rem]" />
                 <div>Wallet</div>
               </li>
-              <li className="flex  px-4 py-2 text-center">
+              <li className="cursor-pointer flex  px-4 py-2 text-center">
                 <LocalShippingIcon className="text-violet-700 mr-2 p-[0.15rem]" />
                 <div>Orders</div>
               </li>
@@ -82,7 +114,10 @@ function Sidebar() {
                 <AccountBoxIcon className="text-violet-700 mr-2 p-[0.15rem]" />
                 <div>Account</div>
               </li>
-              <li className="flex  px-4 py-2 text-center">
+              <li
+                onClick={handleLogout}
+                className="flex hover:bg-slate-200 cursor-pointer px-4 py-2 text-center"
+              >
                 <LogoutIcon className="text-violet-700 mr-2 p-[0.15rem]" />
                 <div>Logout</div>
               </li>
